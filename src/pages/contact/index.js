f6cb2c376fb77d4674f924886d9c5f13417a88d3
Smoook92 <axess-88@mail.ru>
2024-09-18 19:59:13 +0300

    123

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -108,14 +108,11 @@ export const ContactUs = () => {
           </Col>
           <Col lg="5" className="mb-5">
             <div className="social-icons">
-                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                <a href="https://t.me/smoook92/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faTelegram} size="2x" />
                 </a>
-                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faTwitter} size="2x" />
-                </a>
-                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                <a href="https://www.instagram.com/smoook92/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
                 </a>
               </div>
 
